b6b0ae5c46c0a497f4eb65d475b36c6215bd315c
unknown <sadok.sassi@esprit.tn>
2023-04-09 16:08:53 +0100

    affichage evenement front

---
+++ src/Controller/EvenementFrontController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EvenementRepository;
+
+#[Route('/evenements')]
+class EvenementFrontController extends AbstractController
+{
+    #[Route('/', name: 'app_evenement_front')]
+    public function index(EvenementRepository $repo): Response
+    {
+        $evenements = $repo->findAll();
+        return $this->render('evenement_front/index.html.twig', [
+            'evenements' => $evenements,
+            'controller_name' => 'EvenementFrontController',
+        ]);
+    }
+}
