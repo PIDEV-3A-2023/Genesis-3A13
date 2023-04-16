567882730341132e71447eca478aa428c181bc7b
unknown <sadok.sassi@esprit.tn>
2023-04-16 01:58:05 +0100

    details event front

--- src/Controller/EvenementFrontController.php
+++ src/Controller/EvenementFrontController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EvenementRepository;
+use App\Entity\Evenement;
 
 #[Route('/evenements')]
 class EvenementFrontController extends AbstractController
@@ -17,6 +18,15 @@ class EvenementFrontController extends AbstractController
         return $this->render('evenement_front/index.html.twig', [
             'evenements' => $evenements,
             'controller_name' => 'EvenementFrontController',
+        ]);
+    }
+    #[Route('/{idEvenement}', name: 'app_evenement_show_front', methods: ['GET'])]
+    public function show(Evenement $evenement): Response
+    {
+       //$evenement=$repo->findOneBy($idEvenement);
+        return $this->render('evenement_front/show.html.twig', [
+            'evenement' => $evenement,
+            
         ]);
     }
    
