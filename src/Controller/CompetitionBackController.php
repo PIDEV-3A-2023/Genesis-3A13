213755c3d1ca21e63795c67f5b1466613c1fa5bb
malek-labidi <labidimalek@esprit.tn>
2023-04-17 23:22:23 +0100

    downlod pdf with dompdf (not working)

--- src/Controller/CompetitionBackController.php
+++ src/Controller/CompetitionBackController.php
@@ -1,6 +1,9 @@
 <?php
 
+
 namespace App\Controller;
+require_once __DIR__.'/../../vendor/autoload.php';
+
 
 use App\Entity\Competition;
 use App\Repository\CompetitionRepository;
@@ -8,6 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Dompdf\Dompdf;
+use Dompdf\DompdfOptions as Options;
+
+
 #[Route('/competition')]
 class CompetitionBackController extends AbstractController
 {
@@ -28,5 +35,44 @@ class CompetitionBackController extends AbstractController
             'competitions' => $resultat,
         ]);
     }
+   /* #[Route('/download', name: 'app_competition_download', methods: ['GET'])]
+    public function download(Request $request,CompetitionRepository $repo): Response
+    {
+        //définir les options
+        $pdfOptions = new Options();
+        //police par défaut
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->set('isRemoteEnabled', TRUE);
+
+        //instancier Dompdf
+        $pdf = new Dompdf($pdfOptions);
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => FALSE,
+                'verify_peer_name' => FALSE,
+                'allow_self_signed'=> TRUE
+            ]
+        ]);
+        $pdf->setHttpContext($context);
+
+        //générer le html
+        $competitions = $repo->findAll();
+        $html=$this->renderView('competition/download.html.twig', [
+            'competitions' => $competitions,
+        ]);
+
+        $pdf->loadHtml($html);
+        $pdf->setPaper('A4', 'portrait');
+        $pdf->render();
+
+        //générer nom de fichier
+        $fichier = 'maktabti-competitions.pdf';
+        
+        //envoyer le fichier
+        $pdf->stream($fichier, [
+            "Attachment" => true
+        ]);
+        return new Response();
+    }*/
 
 }
