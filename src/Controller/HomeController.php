5bca4fbb2e934b9608aac0a7fc20c86d1e981b00
unknown <sadok.sassi@esprit.tn>
2023-04-23 22:48:20 +0100

    comment diplay+nbr event display front and back

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,24 +2,36 @@
 
 namespace App\Controller;
 
+use App\Repository\CommentaireRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CompetitionRepository;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     
-    public function index(CompetitionRepository $repocomp): Response
+    public function index(CompetitionRepository $repocomp,EvenementRepository $repoevent,CommentaireRepository $repocomm): Response
     {
         $competitions = $repocomp->findAll();
         $nbcompetitions = count($competitions);
+
+        $evenements = $repoevent->findAll();
+        $nbevenements = count($evenements);
+        $comments= $this->comments($repocomm);
         
         return $this->render('front/index.html.twig', [
             'nbcompetitions' => $nbcompetitions,
-            
+            'nbevenements' => $nbevenements,
+            'comments' => $comments,
             'message' => 'Welcome to my homepage!',
         ]);
+    }
+
+    public function comments(CommentaireRepository $repocomm){
+        $comments = $repocomm->findRandomComments();
+        return $comments;
     }
 }
 
