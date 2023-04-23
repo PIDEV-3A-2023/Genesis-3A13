8cc9222734213380eed182f6d068aaf4346ff019
unknown <sadok.sassi@esprit.tn>
2023-04-23 21:08:04 +0100

    ticket not finished

--- src/Controller/EvenementFrontController.php
+++ src/Controller/EvenementFrontController.php
@@ -8,11 +8,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\EvenementRepository;
 use App\Entity\Evenement;
+use App\Entity\Reservation;
+use App\Entity\Ticket;
 use App\Repository\CommentaireRepository;
+use App\Repository\ReservationRepository;
+use App\Repository\TicketRepository;
 use App\Repository\UtilisateurRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Doctrine\ORM\EntityManagerInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 
 #[Route('/evenements')]
@@ -79,21 +85,102 @@ class EvenementFrontController extends AbstractController
     #[Route('/commentaire/edit/{idCommentaire}', name: 'app_commentaire_edit_front')]
     public function editCommentaire(Request $request, Commentaire $commentaire, EntityManagerInterface $entityManager, CommentaireRepository $repocom)
     {
-        
+
         $text = $request->get('commentaireedit');
         $commentaire->setCommentaire($text);
-        
+
         $entityManager->flush();
 
         $this->addFlash('success', 'Votre commentaire a été modifié avec succès!');
 
         return $this->redirectToRoute('app_evenement_show', ['idEvenement' => $commentaire->getIdEvenement()->getIdEvenement()]);
-        
+
 
         return $this->render('evenement/show.html.twig', [
             'evenement' => $commentaire->getIdEvenement(),
-           
+
             'commentaire_id' => $commentaire->getIdCommentaire()
+        ]);
+    }
+    #[Route('/reservation/{idEvenement}', name: 'reservation')]
+    public function reservation(EvenementRepository $repo, $idEvenement)
+    {
+        $evenement = $repo->findOneBy(array('idEvenement' => $idEvenement));
+
+
+        return $this->render('evenement_front/reservation.html.twig', ['evenement' => $evenement]);
+    }
+    #[Route('/ajouterreservation/{idEvenement}', name: 'reservationajout')]
+    public function ajoutreservation(EvenementRepository $repo, $idEvenement, Request $request, ReservationRepository $resrepo, UtilisateurRepository $repouser, TicketRepository $repoticket)
+    {
+        $ticket = new Ticket();
+        $ticket->setType('VIP');
+        $ticket->setPrix(100);
+        $repoticket->save($ticket, true);
+        
+        $evenement = $repo->findOneBy(array('idEvenement' => $idEvenement));
+        $nbticket = $evenement->getNbTicket() - $request->get('nbrTickets');
+
+        $reservation = new Reservation();
+        $evenement->setnbticket($nbticket);
+        $repo->save($evenement, true);
+
+        $reservation->setIdEvenement($evenement);
+        $reservation->setEtat('reserver');
+        $reservation->setIdTicket($ticket);
+        $resrepo->save($reservation, true);
+
+        $pdfResponse = $this->reserveTicket($evenement);
+        return $pdfResponse;
+
+
+        //$this->addFlash('success', 'reservation est effectué avec succés!');
+        //$evenements = $repo->findAll();
+        
+        //return $this->render('evenement_front/index.html.twig', ['evenements' => $evenements]);
+    }
+    public function reserveTicket(Evenement $evenement)
+    {
+       //définir les options
+       $pdfOptions = new Options();
+       //police par défaut
+       $pdfOptions->set('defaultFont', 'Arial');
+       $pdfOptions->set('isRemoteEnabled', TRUE);
+       $pdfOptions->setChroot('');
+
+       //instancier Dompdf
+       $pdf = new Dompdf($pdfOptions);
+       $context = stream_context_create([
+           'ssl' => [
+               'verify_peer' => FALSE,
+               'verify_peer_name' => FALSE,
+               'allow_self_signed'=> TRUE
+           ]
+       ]);
+       $pdf->setHttpContext($context);
+       $img = file_get_contents('Front/images/competition/competition-img.jpg');
+       $imgData = base64_encode($img);
+       $imgSrc = 'data:image/jpeg;base64,' . $imgData;       
+
+        // Render the ticket template using the generated data
+        $html = $this->renderView('evenement_front/ticket.html.twig', [
+            'evenement' => $evenement,
+            'img'=>$imgSrc
+        ]);
+
+        // Generate the PDF from the HTML using dompdf
+        $pdf = new Dompdf();
+        $pdf->loadHtml($html);
+        $pdf->setPaper('A4', 'portrait');
+        $pdf->render();
+
+        // Render the PDF as a string
+        $pdfData = $pdf->output();
+
+        // Return the PDF as a Response object
+        return new Response($pdfData, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="ticket.pdf"',
         ]);
     }
 }
