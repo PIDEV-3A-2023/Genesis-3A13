baa0b8f9da1c88137653c0897a64d77f7a82a2d2
malek-labidi <labidimalek@esprit.tn>
2023-04-27 12:16:06 +0100

    update

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,7 +8,6 @@ use App\Repository\EvenementRepository;
 use App\Repository\UtilisateurRepository;
 use App\Repository\LivreRepository;
 use App\Repository\ResultatQuizRepository;
-use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
