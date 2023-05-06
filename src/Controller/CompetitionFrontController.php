6a63c399aa953c24ab51744154a977ee23e4aa05
malek-labidi <labidimalek@esprit.tn>
2023-05-06 18:19:31 +0100

    api get questions ✅

--- src/Controller/CompetitionFrontController.php
+++ src/Controller/CompetitionFrontController.php
@@ -34,6 +34,11 @@ class CompetitionFrontController extends AbstractController
             'controller_name' => 'CompetitionFrontController',
         ]);
     }
+    /**
+     * api rest json get All 
+     *
+     * 
+     */
     #[Route('/get', name: 'app_competition_front_rest')]
     public function index_rest(CompetitionRepository $repo, BlobExtension $blobExtension): Response
     {
@@ -68,6 +73,9 @@ class CompetitionFrontController extends AbstractController
         // Return the JSON response with the modified competition objects
         return $this->json($data, 200, ['Content-Type' => 'application/json']);
     }
+
+
+
     #[Route('/{idCompetition}', name: 'app_competition_show_front', methods: ['GET'])]
     public function show(Competition $competition): Response
     {
@@ -75,16 +83,23 @@ class CompetitionFrontController extends AbstractController
             'competition' => $competition,
         ]);
     }
+
+
+    /**
+     * api rest json get one by id Competition
+     *
+     * 
+     */
     #[Route('/get/{idCompetition}', name: 'app_competition_show_rest', methods: ['GET'])]
     public function showRest(Competition $competition): Response
     {
-    
+
         $data = [];
         $imageData = null;
         //dd($competition);
-    
+
         if ($competition->getImage() !== null) {
-    
+
             $imageResource = $competition->getImage();
             $imageString = stream_get_contents($imageResource);
             $compressedData = gzcompress($imageString);
@@ -99,21 +114,21 @@ class CompetitionFrontController extends AbstractController
             'lienCompetition' => $competition->getLienCompetition(),
             'recompense' => $competition->getRecompense(),
             'listePaticipants' => $competition->getListePaticipants(),
-            'dateDebut' => $competition->getDateDebut(),
-            'dateFin' => $competition->getDateFin(),
+            'dateDebut' => $competition->getDateDebut()? $competition->getDateDebut()->format('Y-m-d') : null,
+            'dateFin' => $competition->getDateFin()? $competition->getDateFin()->format('Y-m-d') : null,
             'image' => $imageUrl,
-    
+
         ];
-      
+
         return $this->json($data, 200, ['Content-Type' => 'application/json']);
-    
     }
 
 
 
     #[Route('/quiz/{idCompetition}', name: 'app_competition_quiz_front', methods: ['GET'])]
     public function quiz(QuestionRepository $repo, $idCompetition, CompetitionRepository $competitionRepo): Response
-    {$this->denyAccessUnlessGranted('ROLE_USER');
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         $score = 0;
 
         $comp = $idCompetition;
@@ -127,9 +142,46 @@ class CompetitionFrontController extends AbstractController
             'score' => $score,
         ]);
     }
+
+
+
+    /**
+     * api rest json get questions 
+     *
+     * 
+     */
+
+    #[Route('/quiz/get/{idCompetition}', name: 'app_competition_quiz_front_rest', methods: ['GET'])]
+    public function quiz_rest(QuestionRepository $repo, $idCompetition, CompetitionRepository $competitionRepo): Response
+    {
+        $score = 0;
+
+        $comp = intval($idCompetition);
+
+        $questions = $repo->findQuestionsByCompetition($idCompetition);
+        $data = [];
+        //dd($questions);
+        foreach ($questions as $question) {
+            $data[] = [
+                'idQuestion' => $question->getIdQuestion(),
+                'idCompetition' => $comp,
+                'question' => $question->getQuestion(),
+                'choix1' => $question->getChoix1(),
+                'choix2' => $question->getChoix2(),
+                'choix3' => $question->getChoix3(),
+                'reponseCorrect' => $question->getReponseCorrect(),
+            ];
+        }
+
+        return $this->json($data, 200, ['Content-Type' => 'application/json']);
+    }
+
+
+
     #[Route('/quiz/participer/{comp}', name: 'app_competition_participer_front')]
     public function participate(Request $request, QuestionRepository $repo, $comp, UtilisateurRepository $repouser, ResultatQuizRepository $repores, QuizRepository $repoQuiz, competitionRepository $repoComp, Security $security, MailerInterface $mailer): Response
-    {$this->denyAccessUnlessGranted('ROLE_USER');
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         $Client = $security->getUser();
         $idComp = array('idUtilisateur' => $Client);
         $idClient = $repouser->findOneBy($idComp);
@@ -224,4 +276,108 @@ class CompetitionFrontController extends AbstractController
 
         return $this->redirectToRoute('app_competition_front');
     }
+
+    #[Route('/participer/rest/{comp}', name: 'app_competition_participer_front_rest')]
+    public function participateRest(Request $request, QuestionRepository $repo, $comp, UtilisateurRepository $repouser, ResultatQuizRepository $repores, QuizRepository $repoQuiz, competitionRepository $repoComp, Security $security, MailerInterface $mailer): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $Client = $security->getUser();
+        $idComp = array('idUtilisateur' => $Client);
+        $idClient = $repouser->findOneBy($idComp);
+        $questions = $repo->findQuestionsByCompetition($comp);
+
+
+        $score = 0;
+        $reponseClient = '[';
+        $answeredQuestions = 0;
+
+
+
+        foreach ($questions as $question) {
+            $userAnswer = $request->get('question_' . $question->getIdQuestion());
+            $correctAnswer = $question->getReponseCorrect();
+            $reponseClient .=  $userAnswer . ',';
+            if ($userAnswer === $correctAnswer) {
+                $score++;
+            }
+            if (!empty($userAnswer)) {
+                $answeredQuestions++;
+            }
+        }
+
+        $competition = $repoComp->findOneBy(['idCompetition' => $comp]);
+
+        // Check if the user has already participated in the competition
+        $participants = $competition->getListePaticipants();
+        $participantsArray = json_decode($participants, true); // On utilise json_decode pour convertir la liste de participants en tableau associatif
+
+        if (is_array($participantsArray) && in_array($idClient->getIdUtilisateur(), $participantsArray)) {
+            $this->addFlash('danger', 'Vous avez déjà participé à cette compétition.');
+        } else if ($answeredQuestions !== count($questions)) {
+            $this->addFlash('danger', 'Veuillez répondre à toutes les questions avant de soumettre le quiz.');
+            return $this->render('competition_front/quiz.html.twig', [
+                'questions' => $questions,
+                'comp' => $comp,
+                'totalQuestions' => count($questions),
+                'score' => $score,
+            ]);
+        } else {
+            $idQuiz = $repoQuiz->findOneBy(['idCompetition' => $comp]);
+            $reponseClient = rtrim($reponseClient, ',') . ']';
+
+            $resultat = new ResulatQuiz();
+            $resultat->setIdClient($idClient);
+            $resultat->setIdQuiz($idQuiz);
+            $resultat->setScore($score);
+            $resultat->setReponseClient($reponseClient);
+
+            $repores->save($resultat, true);
+
+
+
+            // Check if the user has already participated in the competition
+            $participants = $competition->getListePaticipants();
+
+
+
+            $participantsArray = '[' . $idClient->getIdUtilisateur() . ']' . '' . $participants;
+
+
+            // Update the competition with the new list of participants
+            $competition->setListePaticipants($participantsArray);
+            $repoComp->save($competition, true);
+
+            $email = (new Email())
+                ->from(new Address('maktabti10@gmail.com', 'Maktabti Application'))
+                ->to($idClient->getEmail())
+                ->subject("Confirmation de participation a la compétition : " . $competition->getNom())
+                ->text("Cher/Chère " . $idClient->getNom() . " " . $idClient->getPrenom() . ",\n" .
+                    "\n" .
+                    "Nous tenons à vous remercier d'avoir participé à notre compétition. "
+                    . "Nous sommes ravis que vous ayez décidé de participer et nous espérons que vous avez trouvé l'expérience enrichissante.\n" .
+                    "\n" .
+                    "Nous sommes heureux de vous informer que vos réponses ont été enregistrées avec succès. "
+                    . "Nous avons reçu votre formulaire de réponse et nous sommes impatients de vous donner les résultats dès que possible."
+                    . " Nous vous contacterons dès que nous aurons terminé d'évaluer toutes les réponses.\n" .
+                    "\n" .
+                    "Encore une fois, merci d'avoir participé et d'avoir montré votre intérêt pour notre entreprise. "
+                    . "Nous espérons que vous avez apprécié l'expérience et nous sommes impatients de vous proposer d'autres activités intéressantes à l'avenir.\n" .
+                    "\n" .
+                    "Sincèrement,\n" . "\n" . "\n\n-- \nMaktabti Application \nNuméro de téléphone : +216 52 329 813 \nAdresse e-mail : maktabti10@gmail.com \nSite web : www.maktabti.com");
+
+            $mailer->send($email);
+
+
+
+            $this->addFlash('success', 'votre participation est enregistée avec succés !');
+        }
+
+
+        return $this->redirectToRoute('app_competition_front');
+    }
+
+
+
+
+
 }
