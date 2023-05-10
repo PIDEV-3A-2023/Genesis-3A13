4694615c406801d1a96e1ecbd1eaec8c7de615c5
malek-labidi <labidimalek@esprit.tn>
2023-05-10 11:46:56 +0100

    edit profil fixed with hashpassword

--- src/Controller/ProfileeditController.php
+++ src/Controller/ProfileeditController.php
@@ -3,12 +3,14 @@
 namespace App\Controller;
 use App\Entity\Utilisateur;
 use App\Form\Modifyprofiletype;
+use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfileeditController extends AbstractController
 {
@@ -21,15 +23,27 @@ class ProfileeditController extends AbstractController
     }
 
     #[Route('/{idUtilisateur}/profileedit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Utilisateur $utilisateur, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Utilisateur $utilisateur,UtilisateurRepository $repo, EntityManagerInterface $entityManager,UserPasswordEncoderInterface $passwordHasher): Response
     {
         $form = $this->createForm(Modifyprofiletype::class, $utilisateur);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-            $this->addFlash('success', 'Utilisateur mise à jour avec succés!');
-            return $this->redirectToRoute('app_profileedit', [], Response::HTTP_SEE_OTHER);
+            $utilisateur->setMotDePasse(
+                $passwordHasher->encodePassword(
+                    $utilisateur,
+                    $form->get('motDePasse')->getData()
+                )
+            );
+            $utilisateur->setPassword(
+                $passwordHasher->encodePassword(
+                    $utilisateur,
+                    $form->get('motDePasse')->getData()
+                )
+            );
+            $repo->save($utilisateur, true);
+            $this->addFlash('success', 'Votre compte est mise à jour avec succés!');
+            return $this->redirectToRoute('app_profile_edit', ['idUtilisateur'=> $utilisateur->getIdUtilisateur()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('profileedit/profileedit.html.twig', [
